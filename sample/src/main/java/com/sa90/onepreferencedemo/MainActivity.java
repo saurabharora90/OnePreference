d364e5f291e4edcf2046330886b6871459ad93d5
Saurabh <saurabharora90@gmail.com>
2016-11-20 23:35:42 +0800

    setup basic sample

---
+++ sample/src/main/java/com/sa90/onepreferencedemo/MainActivity.java
@@ -0,0 +1,34 @@
+package com.sa90.onepreferencedemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.sa90.onepreference.utils.OnePreferenceHelper;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnNormal).setOnClickListener(this);
+        findViewById(R.id.btnCustom).setOnClickListener(this);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnNormal:
+                OnePreferenceHelper.startActivity(R.xml.pref_headers, "Settings", true, this);
+                break;
+            case R.id.btnCustom:
+                break;
+        }
+    }
+}
---
+++ sample/src/main/java/com/sa90/onepreferencedemo/fragment/Header1Fragment.java
@@ -0,0 +1,18 @@
+package com.sa90.onepreferencedemo.fragment;
+
+import android.os.Bundle;
+
+import com.sa90.onepreference.fragment.BasePreferenceFragment;
+import com.sa90.onepreferencedemo.R;
+
+/**
+ * Created by saurabharora on 20/11/16.
+ */
+
+public class Header1Fragment extends BasePreferenceFragment {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.pref_header1, rootKey);
+    }
+}
