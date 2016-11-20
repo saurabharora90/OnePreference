710aba5f8a920770ddee9dff0a421219428ce3f4
Saurabh <saurabharora90@gmail.com>
2016-11-20 19:13:14 +0800

    ability to launch the correct activity based on screen size

--- library/src/main/java/com/sa90/onepreference/utils/OnePreferenceHelper.java
+++ library/src/main/java/com/sa90/onepreference/utils/OnePreferenceHelper.java
@@ -2,8 +2,12 @@ package com.sa90.onepreference.utils;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.XmlRes;
 
 import com.sa90.onepreference.PhonePreferenceActivity;
+import com.sa90.onepreference.TabletPreferenceActivity;
 
 /**
  * Created by Saurabh Arora on 20/11/16.
@@ -15,4 +19,28 @@ public class OnePreferenceHelper {
     public static final String EXTRA_SHOW_BACK = "extra_show_back";
     public static final String EXTRA_OVERRIDE_BACK_ICON = "extra_override_back_icon";
     public static final String EXTRA_HEADER_RES = "extra_header_res";
+
+    public static void startActivity(@XmlRes int headerRes, String title, Activity callingActivity) {
+        startActivity(headerRes, title, false, callingActivity);
+    }
+
+    public static void startActivity(@XmlRes int headerRes, String title, boolean showBack, Activity callingActivity) {
+        startActivity(headerRes, title, showBack, -1, callingActivity);
+    }
+
+    public static void startActivity(@XmlRes int headerRes, String title, boolean showBack, @DrawableRes int upIcon, Activity callingActivity) {
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_HEADER_RES, headerRes);
+        intent.putExtra(EXTRA_TITLE, title);
+        intent.putExtra(EXTRA_SHOW_BACK, showBack);
+        if(upIcon!=-1)
+            intent.putExtra(EXTRA_OVERRIDE_BACK_ICON, upIcon);
+
+        if(ScreenUtils.isPhone(callingActivity))
+            intent.setClass(callingActivity, PhonePreferenceActivity.class);
+        else
+            intent.setClass(callingActivity, TabletPreferenceActivity.class);
+
+        callingActivity.startActivity(intent);
+    }
 }
