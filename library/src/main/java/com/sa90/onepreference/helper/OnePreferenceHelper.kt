f08ea6b518037356eb3abfd1866db9ae2322c2b6
Saurabh Arora <saurabharora90@gmail.com>
2020-08-24 01:26:41 +0800

    Use withStyledAttributes ktx extension

--- library/src/main/java/com/sa90/onepreference/helper/OnePreferenceHelper.kt
+++ library/src/main/java/com/sa90/onepreference/helper/OnePreferenceHelper.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.util.TypedValue
 import android.util.Xml
 import androidx.annotation.XmlRes
+import androidx.core.content.withStyledAttributes
 import com.sa90.onepreference.R
 import com.sa90.onepreference.model.Header
 import org.xmlpull.v1.XmlPullParser
@@ -14,12 +15,7 @@ import org.xmlpull.v1.XmlPullParserException
 import java.io.IOException
 
 /**
- * This is the helper for doing the heavy lifting to be able to use the Preference v14 support library to work with both Tablets and Phones
- *
- * The process adopted involves following the standard practise of using a two pane layout for tablets
- * but for phone, we parse the header file to extract all the [androidx.preference.PreferenceFragmentCompat]
- * and then place all those fragments in a Normal Activity inside in a vertically oriented LinearLayout.
- * This allows us to ignore the headers on the Phone view and directly display the Preference List.
+ * The helper parses the header file to create a list of [Header] objects
  */
 object OnePreferenceHelper {
     private const val HEADER_ID_UNDEFINED: Long = -1
@@ -54,7 +50,7 @@ object OnePreferenceHelper {
                             + nodeName + " at " + parser.positionDescription
                 )
             }
-            var curBundle: Bundle? = null
+
             val outerDepth = parser.depth
             while (parser.next().also { type = it } != XmlPullParser.END_DOCUMENT
                 && (type != XmlPullParser.END_TAG || parser.depth > outerDepth)
@@ -64,68 +60,67 @@ object OnePreferenceHelper {
                 }
                 nodeName = parser.name
                 if ("header" == nodeName) {
-                    val header =
-                        Header()
-                    val sa = context.obtainStyledAttributes(
-                        attrs, R.styleable.PreferenceHeader
-                    )
-                    header.id = sa.getResourceId(
-                        R.styleable.PreferenceHeader_id,
-                        HEADER_ID_UNDEFINED.toInt()
-                    ).toLong()
-                    var tv = sa.peekValue(
-                        R.styleable.PreferenceHeader_title
-                    )
-                    if (tv != null && tv.type == TypedValue.TYPE_STRING) {
-                        if (tv.resourceId != 0) {
-                            header.titleRes = tv.resourceId
-                        } else {
-                            header.title = tv.string
+                    context.withStyledAttributes(attrs, R.styleable.PreferenceHeader) {
+                        val header = Header()
+                        var curBundle: Bundle? = null
+
+                        header.id = getResourceId(
+                            R.styleable.PreferenceHeader_id,
+                            HEADER_ID_UNDEFINED.toInt()
+                        ).toLong()
+                        var tv = peekValue(
+                            R.styleable.PreferenceHeader_title
+                        )
+                        if (tv != null && tv.type == TypedValue.TYPE_STRING) {
+                            if (tv.resourceId != 0) {
+                                header.titleRes = tv.resourceId
+                            } else {
+                                header.title = tv.string
+                            }
                         }
-                    }
-                    tv = sa.peekValue(
-                        R.styleable.PreferenceHeader_summary
-                    )
-                    if (tv != null && tv.type == TypedValue.TYPE_STRING) {
-                        if (tv.resourceId != 0) {
-                            header.summaryRes = tv.resourceId
-                        } else {
-                            header.summary = tv.string
+                        tv = peekValue(
+                            R.styleable.PreferenceHeader_summary
+                        )
+                        if (tv != null && tv.type == TypedValue.TYPE_STRING) {
+                            if (tv.resourceId != 0) {
+                                header.summaryRes = tv.resourceId
+                            } else {
+                                header.summary = tv.string
+                            }
                         }
-                    }
-                    header.iconRes = sa.getResourceId(
-                        R.styleable.PreferenceHeader_icon, 0
-                    )
-                    header.fragment = sa.getString(
-                        R.styleable.PreferenceHeader_fragment
-                    )
-                    sa.recycle()
-                    if (curBundle == null) {
-                        curBundle = Bundle()
-                    }
-                    val innerDepth = parser.depth
-                    while (parser.next().also { type = it } != XmlPullParser.END_DOCUMENT
-                        && (type != XmlPullParser.END_TAG || parser.depth > innerDepth)
-                    ) {
-                        if (type == XmlPullParser.END_TAG || type == XmlPullParser.TEXT) {
-                            continue
+                        header.iconRes = getResourceId(
+                            R.styleable.PreferenceHeader_icon, 0
+                        )
+                        header.fragment = getString(
+                            R.styleable.PreferenceHeader_fragment
+                        )
+                        if (curBundle == null) {
+                            curBundle = Bundle()
                         }
-                        val innerNodeName = parser.name
-                        if (innerNodeName == "extra") {
-                            context.resources.parseBundleExtra("extra", attrs, curBundle)
-                            XmlUtils.skipCurrentTag(parser)
-                        } else if (innerNodeName == "intent") {
-                            header.intent =
-                                Intent.parseIntent(context.resources, parser, attrs)
-                        } else {
-                            XmlUtils.skipCurrentTag(parser)
+                        val innerDepth = parser.depth
+                        while (parser.next().also { type = it } != XmlPullParser.END_DOCUMENT
+                            && (type != XmlPullParser.END_TAG || parser.depth > innerDepth)
+                        ) {
+                            if (type == XmlPullParser.END_TAG || type == XmlPullParser.TEXT) {
+                                continue
+                            }
+                            val innerNodeName = parser.name
+                            if (innerNodeName == "extra") {
+                                context.resources.parseBundleExtra("extra", attrs, curBundle)
+                                XmlUtils.skipCurrentTag(parser)
+                            } else if (innerNodeName == "intent") {
+                                header.intent =
+                                    Intent.parseIntent(context.resources, parser, attrs)
+                            } else {
+                                XmlUtils.skipCurrentTag(parser)
+                            }
                         }
+                        if (curBundle.size() > 0) {
+                            header.fragmentArguments = curBundle
+                            curBundle = null
+                        }
+                        target.add(header)
                     }
-                    if (curBundle.size() > 0) {
-                        header.fragmentArguments = curBundle
-                        curBundle = null
-                    }
-                    target.add(header)
                 } else {
                     XmlUtils.skipCurrentTag(parser)
                 }
